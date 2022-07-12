d0fb8bd43678dc34dc4b48f325904ae08b0a6724
Латыф Маркс <osammarks@gmail.com>
2022-07-12 23:16:08 +0600

    project8-1

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,9 +2,6 @@ from rest_framework import permissions
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
-
-    message = 'Изменение чужого контента запрещено!'
-
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user)
